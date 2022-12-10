44928686e4e186d3ea406a637413f7bf8b0a0c12
Daniel Abramov <danielabramov32@gmail.com>
2022-12-10 14:53:32 +0200

    first commit

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,16 @@
+img_size = 48
+# batch_size = 64
+# datagen_train = ImageDataGenerator(horizontal_flip=True)
+# train_generator = datagen_train.flow_from_directory("PATH OF TRAIN FOLDER",
+#                                                     target_size=(img_size,img_size),
+#                                                     color_mode="grayscale",
+#                                                     batch_size=batch_size,
+#                                                     class_mode='categorical',
+#                                                     shuffle=True)
+# datagen_validation = ImageDataGenerator(horizontal_flip=True)
+# validation_generator = datagen_validation.flow_from_directory("PATH OF TEST FOLDER",
+#                                                     target_size=(img_size,img_size),
+#                                                     color_mode="grayscale",
+#                                                     batch_size=batch_size,
+#                                                     class_mode='categorical',
+#                                                     shuffle=False)
